6f5fa705a6a887ec662c890b5cc561e5ec9521a7
lining0806 <lining0806@gmail.com>
2015-07-28 10:22:18 +0800

    test

--- TextFilter.py
+++ TextFilter.py
@@ -43,7 +43,7 @@ def TextSeg(text, lag):
         #-------------------------------------------------------------------------------
     return word_list
 
-class MongoDBIO:
+class MongoDBIO(object):
     # 申明相关的属性
     def __init__(self, host, port, name, password, database, collection):
         self.host = host
@@ -55,23 +55,30 @@ class MongoDBIO:
 
     # 连接数据库，db和posts为数据库和集合的游标
     def Connection(self):
-        # ##'''1.Connection'''
+        ## --------------------------------------------------------------------------------
         # # connection = pymongo.Connection() # 连接本地数据库
         # connection = pymongo.Connection(host=self.host, port=self.port)
         # db = connection[self.database]
-        # if self.name or self.password:
+        # if len(self.name)>0:
         #     db.authenticate(name=self.name, password=self.password) # 验证用户名密码
-        # print "Database:", db.name
-        ##'''2.MongoClient'''
-        ## mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
-        uri = "mongodb://%s:%s@%s:%d/%s" % (self.name, self.password, self.host, self.port, self.database)
-        print uri
+        # else:
+        #     pass
+        ## --------------------------------------------------------------------------------
+        # mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
+        if len(self.name)>0:
+            uri = "mongodb://%s:%s@%s:%d/%s" % (self.name, self.password, self.host, self.port, self.database)
+        else:
+            uri = "mongodb://%s:%d/%s" % (self.host, self.port, self.database)
+        # print uri
         client = pymongo.MongoClient(uri)
         db = client.get_default_database()
-
+        ## --------------------------------------------------------------------------------
+        print "Database:", db.name
+        print db.collection_names() # 查询所有集合
         posts = db[self.collection]
         print "Collection:", posts.name
         print posts.count()
+
         return posts
 
 def DataFliter(host, port, name, password, database, collection, Limit_Number, lag, stopwords_set, content_column, time_column, SendMailConfig=[]):
